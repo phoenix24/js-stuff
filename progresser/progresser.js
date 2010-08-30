9db3c52d41dbf8cdc19c208499051f9ce143d3f7
chaitanya <gopi.daiict@gmail.com>
2010-08-30 20:55:51 +0530

    playing with the twitter search results

---
+++ progresser/progresser.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("form").submit(function(){
+    var query = $("#search").val();
+    var url = "http://search.twitter.com/search.json?callback=?&q="+query
+    
+    $("p#twt").remove();
+
+    $.getJSON(url, function(json){
+        $.each(json.results, function(i, twt){
+            $("#result").append('<p id="twt"> <img src=' + twt.profile_image_url + '>'+twt.text+'</p>');
+        });
+    }); 
+    return false;
+    });
+});
